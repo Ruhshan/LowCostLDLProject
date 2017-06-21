7ae8e120ae2556aa49e95d77b2c0b309a4b87bab
Ruhshan <ruhshan.ahmed@gmail.com>
2017-06-22 02:21:07 +0600

    static file storage disabled

--- IcddrbProject/settings.py
+++ IcddrbProject/settings.py
@@ -154,4 +154,4 @@ LOGIN_REDIRECT_URL = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
