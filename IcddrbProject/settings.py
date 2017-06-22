c09833044d31ebd1d9d9e9e7f80a6b8e416ad6a4
Ruhshan <ruhshan.ahmed@gmail.com>
2017-06-22 10:46:33 +0600

    heroku configured with settings_ and local settings have local db

--- IcddrbProject/settings.py
+++ IcddrbProject/settings.py
@@ -107,13 +107,13 @@ DATABASES = {
 
 
 #heroku db
-# SECRET_KEY = config('SECRET_KEY')
-#
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
+SECRET_KEY = config('SECRET_KEY')
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
